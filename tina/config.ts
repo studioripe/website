8b2b5592b518f94c64613cc953eb9d884f1e2a23
Joshua Mangiola <joshm982@gmail.com>
2023-04-25 11:12:53 +0930

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -32,13 +32,6 @@ export default defineConfig({
             required: true,
           },
           {
-            name: "draft",
-            label: "Draft",
-            type: "boolean",
-            required: true,
-            description: "If this is checked the post will not be published",
-          },
-          {
             type: "datetime",
             name: "date",
             label: "Date",
